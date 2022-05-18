46a552e705bab3a321d84caba739a42ca6047770
swathi.palamani <swathi.palamani@DT150159.QICHOAPPL.com>
2022-05-18 15:03:50 +0530

    may18

---
+++ lab1/src/lab1/Switch.java
@@ -0,0 +1,25 @@
+package lab1;
+
+public interface Switch {
+	
+	public static void main(String[] args) {
+		int tech=2;
+		switch (tech) {
+		case 1:
+		{
+		System.out.println("java");
+		break;
+		}
+		case 2:
+		{
+			System.out.println("ES6");
+			break;
+		}
+		default:
+		{
+			System.out.println("Not Listed...");
+		}
+	}
+
+	}
+}
