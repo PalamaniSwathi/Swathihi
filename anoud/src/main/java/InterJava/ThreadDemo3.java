a7b38abfdb065a8538f226df8f2c1af44ad7a978
swathi.palamani <swathi.palamani@DT150159.QICHOAPPL.com>
2022-06-13 14:02:34 +0530

    13th june

--- anoud/src/main/java/InterJava/ThreadDemo3.java
+++ anoud/src/main/java/InterJava/ThreadDemo3.java
@@ -6,11 +6,11 @@ public class ThreadDemo3 {
 		ExecutorService es=Executors.newFixedThreadPool(1);
 		es.execute(()->{System.out.println("child thread called.....");});
 		es.shutdown();
-		System.out.println("childthread...");
+		//System.out.println("childthread...");
 	}
 	public static void main(String[] args) {
 		new ThreadDemo3();
-		for(int i=0;i<=5;i++) {
+		for(int i=0;i<5;i++) {
 			System.out.println(i);
 			try {
 				Thread.sleep(1000);
