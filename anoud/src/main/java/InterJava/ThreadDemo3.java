ee886ddee5989d1efecda67eace01ab616f98bb1
swathi.palamani <swathi.palamani@DT150159.QICHOAPPL.com>
2022-06-13 14:44:42 +0530

    13th june

--- anoud/src/main/java/InterJava/ThreadDemo3.java
+++ anoud/src/main/java/InterJava/ThreadDemo3.java
@@ -6,7 +6,7 @@ public class ThreadDemo3 {
 		ExecutorService es=Executors.newFixedThreadPool(1);
 		es.execute(()->{System.out.println("child thread called.....");});
 		es.shutdown();
-		//System.out.println("childthread...");
+		//System.out.println("child thread...");
 	}
 	public static void main(String[] args) {
 		new ThreadDemo3();
