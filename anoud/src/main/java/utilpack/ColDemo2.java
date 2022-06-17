6204f2f9176456cc1790041ca0127a1193c46752
swathi.palamani <swathi.palamani@DT150159.QICHOAPPL.com>
2022-06-17 13:50:37 +0530

    17th june

---
+++ anoud/src/main/java/utilpack/ColDemo2.java
@@ -0,0 +1,43 @@
+package utilpack;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.function.Consumer;
+
+public class ColDemo2 {
+public static void main(String[] args) {
+	Set<String> hs=new HashSet<String>();
+	
+	hs.add("aaa");
+	hs.add("baa");
+	hs.add("caa");
+	
+	System.out.println(hs);
+	hs.remove("aaa");
+	System.out.println(hs.size());
+	System.out.println(hs);
+	System.out.println(hs.isEmpty());
+	System.out.println(hs.contains("baa"));
+	//hs.forEach(System.out::println);
+	//hs.forEach(new MyConsumer());
+	//hs.forEach((s)->{System.out.println);
+	hs.forEach(ColDemo2::print);
+	Iterator<String> it=hs.iterator();
+	while(it.hasNext()) {
+		System.out.println(it.next());
+	}
+}
+ static void print(String s) {
+	 System.out.println(s);
+	
+}
+}
+class MyConsumer implements Consumer<String>{
+	@Override
+	public void accept(String s) {
+		System.out.println(s);
+		
+	}
+	
+}
