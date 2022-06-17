0e78f0ef44e3b20598ce2f84373d5ef52567e6df
swathi.palamani <swathi.palamani@DT150159.QICHOAPPL.com>
2022-06-17 14:07:38 +0530

    17th june

--- anoud/src/main/java/utilpack/ColDemo2.java
+++ anoud/src/main/java/utilpack/ColDemo2.java
@@ -9,19 +9,20 @@ public class ColDemo2 {
 public static void main(String[] args) {
 	Set<String> hs=new HashSet<String>();
 	
-	hs.add("aaa");
-	hs.add("baa");
-	hs.add("caa");
+	hs.add("aa");
+	hs.add("ba");
+	hs.add("ca");
 	
 	System.out.println(hs);
-	hs.remove("aaa");
+	hs.remove("aa");
 	System.out.println(hs.size());
 	System.out.println(hs);
 	System.out.println(hs.isEmpty());
-	System.out.println(hs.contains("baa"));
+	System.out.println(hs.contains("ba"));
+	
 	//hs.forEach(System.out::println);
 	//hs.forEach(new MyConsumer());
-	//hs.forEach((s)->{System.out.println);
+	//hs.forEach((s)->{System.out.println(s);});
 	hs.forEach(ColDemo2::print);
 	Iterator<String> it=hs.iterator();
 	while(it.hasNext()) {
