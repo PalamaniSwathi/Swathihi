efd5da14c0fca07461ee941c7d9c2b0b524edb1a
swathi.palamani <swathi.palamani@DT150159.QICHOAPPL.com>
2022-06-24 15:51:41 +0530

    24 june

---
+++ anoud/src/main/java/Jdbcpack/JDBCDemo4.java
@@ -0,0 +1,25 @@
+package Jdbcpack;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class JDBCDemo4 {
+	public static void main(String[] args)throws Exception {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/anound","root","root");
+		
+		System.out.println(con);
+		String sql="select * from users";
+		Statement stmt=con.createStatement();
+		
+		ResultSet rs=stmt.executeQuery(sql);
+		
+		while(rs.next()) {
+			System.out.println(rs.getInt("uid")+":"+rs.getString("uname"));
+		}
+		
+	}
+
+}
